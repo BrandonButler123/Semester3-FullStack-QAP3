9a580fa56e68801195bdfe6280869d6c8141dd88
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-03-17 19:06:47 -0230

    initializing the index page

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
