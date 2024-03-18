af3822a6ac5bac7c193a052bfb11d49a906d366f
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-03-18 13:32:22 -0230

    server/route setup

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const methodOverride = require("method-override");
 const app = express();
-const usersRouter = require("./routes/users");
 
-const port = 3000;
-
-global.DEBUG = true;
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
+// Routes
+const apiRoutes = require("./routes/apiRoutes");
+const webRoutes = require("./routes/webRoutes");
+app.use("/api", apiRoutes);
+app.use("/", webRoutes);
 
-app.use("/index", usersRouter);
-
-// app.get("/users", (req, res) => {
-//   res.render("users");
-// });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
